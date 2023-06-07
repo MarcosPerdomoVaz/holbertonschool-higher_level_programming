30c1760494bd87dc90979d8f01cc5cc523df76ca
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-06-07 14:28:48 -0700

    fix task 10

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        for i in reversed(my_list):
-            print("{:d}".format(i))
+def divisible_by_2(my_list=[]):
+    return[x % 2 == 0 for x in my_list]
