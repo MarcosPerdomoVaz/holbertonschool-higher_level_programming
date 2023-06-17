87d85b5dadb86b51a7fc41994ada3e291e3ea405
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-06-17 11:51:00 -0700

    fix task 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+
+"""author: MarcosPerdomoVaz"""
 class Square:
+    """Defines a square"""
     def __init__(self, size=0):
+        """Initialises the data"""
         self.size = size
 
     def area(self):
+        """Returns current square area"""
         return self.__size**2
 
     @property
     def size(self):
+        """Getter method"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Setter method"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -19,6 +26,7 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        """Prints the square"""
         if self.__size == 0:
             print()
         else:
