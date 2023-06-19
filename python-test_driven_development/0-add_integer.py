170af61cf4ae13557be704392e12463658e8bc7a
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-06-18 17:39:59 -0700

    task 1

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Insert here module comment"""
+"""
+Insert here module comment
+"""
 
 
 def add_integer(a, b=98):
-    """ add two integers or flots """
+    """
+    add two integers or flots
+    """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
