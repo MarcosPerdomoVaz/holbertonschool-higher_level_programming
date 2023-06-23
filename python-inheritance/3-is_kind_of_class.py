72b8b6dd56bb70502a5ee7a0ceaaa6c6d13522f4
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-06-23 15:36:36 -0700

    tasks from 0 to 6

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Same class or inherit method checker
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if object is instance of args
+    """
+
+    return isinstance(obj, a_class)
