1bccb584b7344726b6142f4451611dd661cd89ca
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-06-23 15:47:34 -0700

    tasks from 7 to 11

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,10 +6,5 @@ Attribute lookup
 
 def lookup(obj):
     """Lookup all attributes in Class
-
-    Args:
-        obj: object class
-    Returns:
-        Na
     """
     return [i for i in dir(obj)]
--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,11 +6,6 @@ Method to check class
 
 def is_same_class(obj, a_class):
     """check for class if same type of object
-    args:
-        obj: object import
-        a_class: class type
-    Return:
-        True or False
     """
 
     if type(obj) is a_class:
