13fc1b603c9b98de974bf90e3914ce078e7a6c43
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-06-24 19:03:23 -0700

    fix task 1

--- python-input_output/1-number_of_lines.py
+++ python-input_output/1-number_of_lines.py
@@ -2,12 +2,7 @@
 """ Write a function that writes a string to a text file """
 
 
-def number_of_lines(filename=""):
-    """
-    must use the with
-    """
-    line_num = 0
-    with open(filename, encoding="utf-8") as f:
-        for line in f:
-            line_num += 1
-    return line_numi
+def write_file(filename="", text=""):
+    """ must use the with """
+    with open(filename, 'w') as f:
+        return f.write(text)
