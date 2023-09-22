328e5885d3a42f59ae59cefdf4e16c5d12ed8c05
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-09-22 15:26:15 -0700

    fix task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for comb2 in range(0, 99):
-    print('{:02}'.format(comb2), end=', ' if comb2 != 99 else '')
-print('{}'.format(99))
+for n in range(0, 99):
+    print("{:02d}".format(n), end=",")
+print("99")
