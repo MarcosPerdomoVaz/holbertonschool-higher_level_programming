bb450c4600d1a95d2a0232a295b76e54de9c9d19
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-09-22 15:29:23 -0700

    fix task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for abc in range(0, 10):
-    for cba in range(abc + 1, 10):
-        if ((abc != 8 or cba != 9) and cba > abc):
-            print('{:01d}{:01d}'.format(abc, cba), end=', ')
-print('89')
+for tens in range(0, 10):
+    for ones in range(0, 10):
+        if ones > tens and (ones + tens) < 17:
+            print("{:d}{:d}".format(tens, ones), end=", ")
+print("89")
