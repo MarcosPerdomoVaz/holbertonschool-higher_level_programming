74a9b280872f50c9096fea4e4db4f177697369e7
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-09-23 15:53:48 -0700

    fix task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+
+    def find():
+        names = dir(hidden_4)
+        for i in names:
+            if not i.startswith('__'):
+                print('{}'.format(i))
+find()
