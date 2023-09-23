26477f64f10ab3f798c30f52edd1d6013acf3068
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-09-23 15:49:32 -0700

    fix task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    i = len(sys.argv) - 1
 
-    def pargs(argv):
-        n = len(argv) - 1
-        if n > 1:
-            print('{} arguments:'.format(n))
-        elif n == 1:
-            print('{} argument:'.format(n))
-        else:
-            print('{} arguments.'.format(n))
-        for i in range(1, n + 1):
-            print("{}: {}".format(i, argv[i]))
-pargs(sys.argv)
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
