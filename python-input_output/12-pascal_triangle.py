54010868e4032089cf2aafa042569a1fd0bfaf80
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-10-08 16:40:16 -0700

    fix task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
+
 def pascal_triangle(n):
-    """ Function that returns the pascal triangle
+    """Returns a list of lists of integers representing the Pascal's triangle of
+    `n`.
 
     Args:
-        n: number of lines
-
-    Returns:
-        matrix: a matrix with the pascal triangle
+        n (int): base number for calculating triangle
 
     """
-
-    matrix = []
-    prev = []
-
-    for i in range(n):
-        res_list = []
-        p1 = -1
-        p2 = 0
-        for j in range(len(prev) + 1):
-            if p1 == -1 or p2 == len(prev):
-                res_list += [1]
-            else:
-                res_list += [prev[p1] + prev[p2]]
-            p1 += 1
-            p2 += 1
-        matrix.append(res_list)
-        prev = res_list[:]
-
-    return matrix
+    triangle = []
+    if n <= 0:
+        return triangle
+    for row in range(n):
+        new_row = []
+        triangle.append(new_row)
+        new_row.append(1)
+        for col in range(1, row):
+            new_row.append(triangle[row - 1][col - 1] + triangle[row - 1][col])
+        if row > 0:
+            new_row.append(1)
+    return triangle
