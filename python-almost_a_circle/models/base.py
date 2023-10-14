c66664879176ef78a3ff9afad956a154cb054682
MarcosPerdomoVaz <6331@holbertonstudents.com>
2023-10-14 16:33:37 -0700

    fix task 18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,3 +40,13 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+     @classmethod
+    def create(cls, **dictionary):
+        '''prints instances'''
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
